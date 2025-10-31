0302462a984eaa9a8c1ad52e35489427e71da1dd
Saikat kole <saikatkole38@gmail.com>
2025-10-31 16:07:00 +0530

    production code ecommerce-app

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ connectDB();
 const app = express();
 
 // middlewares
-app.use(cors());
+app.use(cors({
+  origin: ["https://your-frontend.vercel.app"],
+  credentials: true,
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(morgan('dev'));
